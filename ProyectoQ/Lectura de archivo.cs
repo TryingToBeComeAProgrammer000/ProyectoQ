23de9344fec0b5f249c67ef0e46ac9fec1093dc6
wandererRRRR <josuedavidsolisgamboa@gmail.com>
2023-08-13 00:51:00 -0600

    añadi lo de la clase

--- ProyectoQ/Lectura de archivo.cs
+++ ProyectoQ/Lectura de archivo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace ProyectoQ
@@ -7,11 +8,25 @@ namespace ProyectoQ
     internal class LeerArchivo
     {
         private string _path;
+        private List<string> nombres = new List<string>();          
 
        //constructor de la clase ojo
         public LeerArchivo(string path)
         {
             _path = path;
         }
+        //metodo para leer el archivo
+        public void LeerNombres()
+        {
+            using (StreamReader sr = new StreamReader (_path))
+            {
+              while (sr.EndOfStream != true)
+                {
+                 nombres.Add(sr.ReadLine());
+
+                }
+            }
+        }
+
     }
 }
